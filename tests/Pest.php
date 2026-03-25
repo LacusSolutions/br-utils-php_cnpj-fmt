6283fc9580ca6f8599e2ce583563cb60ba3808bb
juliolmuller <juliolmuller@outlook.com>
2026-03-25 16:07:55 -0300

    chore(cnpj-fmt): migrate test runner from PhpUnit to Pest
    BREAKING CHANGE: PHP v8.1 is no longer supported. Package will now support PHP v8.2+.

---
+++ tests/Pest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+|--------------------------------------------------------------------------
+| Pest bootstrap
+|--------------------------------------------------------------------------
+*/
+
+use PHPUnit\Framework\TestCase;
+
+uses(TestCase::class)->in(__DIR__ . DIRECTORY_SEPARATOR . 'Specs');
